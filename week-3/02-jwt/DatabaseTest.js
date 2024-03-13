421be3d35ee1989b275c029eca97edee234772d0
Aditya raj <meadityaraj0001@gmail.com>
2024-03-13 23:03:59 +0530

    started with second lec assign

---
+++ week-3/02-jwt/DatabaseTest.js
@@ -0,0 +1,43 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const app=express()
+
+app.use(express.json())
+
+mongoose.connect("mongodb://localhost:27017/TestDB1")   
+const User = mongoose.model('Users', { name: String, email: String, password: String});
+const user = new User({ name: 'bhaijaan',email:"bhai@gmail.com",password:"xyz" });
+user.save()
+app.get("/",(req,res)=>{
+    console.log("hello world")
+    res.send({msg:"hello"})
+})
+
+app.post('/signup', async function(req,res){
+
+    const name=req.body.name
+    const email=req.body.email
+    const password=req.body.password
+
+    const existingUser=await User.findOne({email:email})
+
+    if(existingUser)return res.status(400).send("email used in registration already exists!")
+
+    const user=new User({
+        name:name,
+        email:email,
+        password:password
+    })
+    user.save()
+    res.json({
+       "msg" :"user saved successfully"
+    })
+
+})
+
+
+
+
+app.listen(3000,()=>{
+    console.log("listening to requests  on port 3000! hit em!!")
+})
